c7efbad800246ba1b49a1d57c81c90ff41ef7aa2
NguyenThanhHuy2367 <nguyenthanhhuy12a12023@gmail.com>
2024-08-17 14:07:40 +0700

    Add files via upload

---
+++ Khoangcachxagan.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 13:21:35 2024
+
+@author: NGUYEN THANH HUY
+"""
+
+duongdentruong =float(input(" Nhap do dai duong den truong(m):"))
+if duongdentruong <300:
+    print("Duong den truong qua gan. Thoi!di bo")
+elif duongdentruong > 1200:
+    print("Duong den truong qua xa. di xe may")
+elif duongdentruong >= 300 and duongdentruong <= 700:
+    print("duong den truong khong xa. Thoi
---
+++ untitled0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 14:02:53 2024
+
+@author: NGUYEN THANH HUY
+"""
+
+print("hello world")
+duongtoitruong =float(input("do dai don duong den truong(m):"))
+if duongtoitruong>1200:
+    print("duongdentruongquaxa.")
+else:
+    print("khongxacdinhduocxagan.")
+
+    
+
+    
